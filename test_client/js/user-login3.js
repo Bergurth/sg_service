68179d5ef7654ab7a727059b103e2f97deb08cb0
bergur <bergur@namsmat.is>
2015-09-01 14:03:43 +0000

    local client updated to newest sg_client

--- test_client/js/user-login3.js
+++ test_client/js/user-login3.js
@@ -4,28 +4,72 @@
      $('.signup-ctrls').hide();
      $('.password-reset-ctrls').hide();
      $('.update-ctrls').hide();
+     $('.get-ugames-ctrls').hide();
 
      $("#signup-link").click(_.bind(__onSignupLinkClicked));
      $("#update-link").click(_.bind(__onUpdateLinkClicked));
+     $("#signin-link").click(_.bind(__onLoginLinkClicked));
 
+     $("#banner-update-link").click(_.bind(__onUpdateBannerLinkClicked));
+     $("#banner-ugame-link").click(_.bind(__onUgameBannerLinkClicked));
 
      $signupBtn = $("#signup-submit");
      $signinBtn = $("#signin-submit");
      $updateBtn = $("#update-submit");
+     $ugameBtn  = $("#ugame-submit");
+
+     //var server_url = "http://localhost:12315"
+     var server_url = "http://sgtest.bergur.biz";
+
+     $("#user-index-link").attr("href", server_url);
 
      var url1      = window.location.href;
      //location = window.location.href;
-     var source_url = "http://localhost:12315/auth/logout?from_page="+url1;
+     var source_url = server_url +"/auth/logout?from_page="+url1;
      $('#logout_link').attr("href", source_url);
 
 
      $(".auth-form").submit(_.bind(this.__onFormSubmitted, this));
      $(".btn-login").click(_.bind(__onLoginButtonClicked));
      $('#password-reset-link').click(_.bind(__onPasswordResetLinkClicked));
+     $('#cancel-reset-link').click(_.bind(__onLoginLinkClicked));
 
      function __onLoginLinkClicked(event) {
+         $('.signup-ctrls').hide();
+         $('.password-reset-ctrls').hide();
+         $('.update-ctrls').hide();
+         $('.get-ugames-ctrls').hide();
+         $('.signin-ctrls').fadeIn()
           console.log("__onModalShown")
           $('div.signin-ctrls [name=username]').focus();
+      }
+
+      function __onUgameBannerLinkClicked(event){
+        //$("#ugames-uname").val()
+        $('.signup-ctrls').hide();
+         $('.password-reset-ctrls').hide();
+         $('.signin-ctrls').hide();
+         $('.update-ctrls').hide();
+        $("#auth-form-modal").modal('show');
+        $('.get-ugames-ctrls').fadeIn();
+        $('div.get-ugames-ctrls [name=username]').focus();
+      }
+
+      function getUgame(){
+        var username = $("#ugames-uname").val();
+        var url = server_url + "/user/?username="+username;
+        window.location.href = url;
+      }
+
+
+      function __onUpdateBannerLinkClicked(event){
+        $('.signup-ctrls').hide();
+         $('.password-reset-ctrls').hide();
+         $('.signin-ctrls').hide();
+         $('.get-ugames-ctrls').hide();
+        $("#auth-form-modal").modal('show');
+        $('.update-ctrls').fadeIn();
+        $('div.update-ctrls [name=username]').focus();
       }
 
       function __onFormSubmitted(event) {
@@ -39,6 +83,8 @@
               submitAsSignUp($source);
           else if ($updateBtn.is(':visible'))
               submitAsUpdate($source);
+          else if ($ugameBtn.is(':visible'))
+                getUgame();
 
           else {
               $('#password-reset-loader').show();
@@ -46,10 +92,18 @@
           }
       }
 
+      function __onPasswordResetLinkClicked(event) {
+          event.preventDefault();
+
+          $('.signin-ctrls, .signup-ctrls, .get-ugames-ctrls').hide();
+          $('.password-reset-ctrls').fadeIn()
+              .find('[name=email]').focus();
+      }
+
       function __onSignupLinkClicked(event) {
           event.preventDefault();
 
-          $('.signin-ctrls, .password-reset-ctrls .update-ctrls').hide();
+          $('.signin-ctrls, .password-reset-ctrls .update-ctrls .get-ugames-ctrls').hide();
           $('.signup-ctrls').fadeIn()
               .find('[name=email]').focus();
       }
@@ -57,7 +111,7 @@
       function __onUpdateLinkClicked(event) {
             event.preventDefault();
 
-            $('.signin-ctrls, .password-reset-ctrls .signup-ctrls').hide();
+            $('.signin-ctrls, .password-reset-ctrls .signup-ctrls .get-ugames-ctrls').hide();
             $('.update-ctrls').fadeIn()
               .find('[name=username]').focus();
 
@@ -66,11 +120,12 @@
       function __onLoginButtonClicked(event) {
 
           event.preventDefault();
-          console.log("vampira");
-          this.$el.modal();
+          //console.log("vampira");
+          //this.$el.modal();
       }
 
     function submitAsSignUp($form) {
+        $('.signup-ctrls .server-message strong').text("");
         data = JSON.stringify({
             'email'    : $form.find("div.signup-ctrls [name=email]").val(),
             'username' : $form.find("div.signup-ctrls [name=username]").val(),
@@ -79,7 +134,7 @@
         });
 
         $.ajax({
-            url: 'http://localhost:12315/auth/login',
+            url: server_url+'/auth/login',
             type: 'POST',
             contentType: 'application/json',
             data: data,
@@ -91,15 +146,35 @@
             success: function(data){
                 console.log(data);
                 console.log("device control succeeded");
+                if(String(data).length >= 3){
+                __onSignupFailed(data);
+                }
+                else {
+                    $("#username-button").text($form.find("div.signup-ctrls [name=username]").val());
+                    $("#auth-form-modal").modal('hide');
+                    var u_info_url = server_url + "/user/?username="+$form.find("div.signup-ctrls [name=username]").val();
+                    $("#user-info-link").attr("href", u_info_url);
+                }
             },
-            error: function(){
+            error: function(data){
                 console.log(data);
                 console.log("Device control failed");
+                __onSignupFailed(data);
             },
             //processData: false,
             // origin : chrome-extention://mkhojklkhkdaghjjfdnphfphiaiohkef
         });
     }
+
+
+    function __onSignupFailed(errors) {
+           var $signupCtrls = $('.signup-ctrls');
+           var errorstr = String(errors);
+
+           $('.signup-ctrls .server-message strong').text("");
+           $('.signup-ctrls .server-message strong').text(errorstr);
+
+       }
 
      function submitAsUpdate($form) {
         console.log("inside submit as update");
@@ -114,7 +189,7 @@
         });
 
         $.ajax({
-            url: 'http://localhost:12315/state_update',
+            url: server_url+'/state_update',
             type: 'POST',
             contentType: 'application/json',
             xhrFields: {
@@ -145,7 +220,7 @@
         jsn = JSON.stringify({'email': $form.find('div.password-reset-ctrls [name=email]').val()});
 
         $.ajax({
-            url: 'http://localhost:12315/auth/openid_reset',
+            url: server_url+'/auth/openid_reset',
             type: 'POST',
             contentType: 'application/json',
             data: jsn,
@@ -177,7 +252,7 @@
 
         //console.log(data)
         $.ajax({
-            url: 'http://localhost:12315/auth/login',
+            url: server_url+'/auth/login',
             type: 'POST',
             contentType: 'application/json',
             xhrFields: {
@@ -188,6 +263,15 @@
             success: function(data){
                 console.log(data);
                 console.log("device control succeeded");
+                if(String(data).length >= 15){
+                __onSignupFailed(data);
+                }
+                else {
+                    $("#username-button").text($form.find("div.signin-ctrls [name=username]").val());
+                    $("#auth-form-modal").modal('hide');
+                    var u_info_url = server_url + "/user/?username="+$form.find("div.signin-ctrls [name=username]").val();
+                    $("#user-info-link").attr("href", u_info_url);
+                }
             },
             error: function(){
                 console.log("Device control failed");
@@ -211,13 +295,7 @@
       }
 
 
-       function __onPasswordResetLinkClicked(event) {
-          event.preventDefault();
 
-          this.$('.signin-ctrls, .signup-ctrls').hide();
-          this.$('.password-reset-ctrls').fadeIn()
-              .find('[name=email]').focus();
-      }
 
 
       /*
